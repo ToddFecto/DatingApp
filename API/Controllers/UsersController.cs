ba9c4b3fcdfe8f35796e56056f9066c39ab2b1eb
ToddFecto <todd3966@roadrunner.com>
2023-04-17 11:28:58 -0400

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,62 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]  //  GET method /api/users
+
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+
+        }
+    /*  Synchronous request
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers() 
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id); // another simpler, cleaner code way to return the single user
+
+           // return user; one way to return the single user
+        }  
+        
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id); // another simpler, cleaner code way to return the single user
+
+           // return user; one way to return the single user
+        } End Synchronous request */
+
+        // Asynchronous Request
+         [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return  await _context.Users.FindAsync(id); // another simpler, cleaner code way to return the single user
+
+           // return user; one way to return the single user
+        }
+    }
+}
